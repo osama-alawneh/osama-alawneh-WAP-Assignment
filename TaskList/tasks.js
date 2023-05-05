50d679e01102bc229f1e88a33fdb4ce30100dc03
Osama Alawneh <h.a.alawneh9@gmail.com>
2023-05-05 15:07:42 -0500

    Adding tip calculator

---
+++ TaskList/tasks.js
@@ -0,0 +1,26 @@
+const task = document.querySelector('#task');
+const addTaskBtn = document.querySelector("#addTask");
+const clearTasksBtn = document.querySelector("#clearTasks");
+const tasksArea = document.querySelector('#tasks');
+
+addTask.addEventListener("click",addTaskHandler);
+clearTasksBtn.addEventListener("click",clearTasksHandler);
+
+
+function addTaskHandler(){
+    const taskText = task.value
+    tasksArea.value += taskText + '\n'
+    localStorage.setItem("tasks") = tasksArea.value;
+}
+
+function clearTasksHandler(){
+    tasksArea.value = "";
+    localStorage.clear()
+}
+
+window.onload = function() {
+    var storedData = localStorage.getItem("tasks");
+    if (storedData) {
+        tasksArea.value = storedData
+    }
+  }
